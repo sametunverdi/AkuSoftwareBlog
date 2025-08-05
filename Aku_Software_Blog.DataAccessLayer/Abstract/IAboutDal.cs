edef39ecfd7a2b5a698745b262b5bd7fd589e06d
tnyylz <tunayylz22@gmail.com>
2025-08-05 22:13:18 +0300

    interface oluştuu

---
+++ Aku_Software_Blog.DataAccessLayer/Abstract/IAboutDal.cs
@@ -0,0 +1,14 @@
+﻿using Aku_Software_Blog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aku_Software_Blog.DataAccessLayer.Abstract
+{
+    public interface IAboutDal:IGenericDal<About>
+    {
+    
+    }
+}
---
+++ Aku_Software_Blog.DataAccessLayer/Abstract/IMessageDal.cs
@@ -0,0 +1,13 @@
+﻿using Aku_Software_Blog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aku_Software_Blog.DataAccessLayer.Abstract
+{
+    public interface IMessageDal: IGenericDal<Message>
+    {
+    }
+}
